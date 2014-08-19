17929daa48f2fe5e7a4b5620ca8d45da7059f19f
PRN <neelakandan@multicorewareinc.com>
2014-08-19 16:09:01 +0500

    Spacing and Alignment modifications

--- Storage.cpp
+++ Storage.cpp
@@ -2,6 +2,7 @@
 #include "amp.h"
 extern "C"
 {
+
 #include "THFile.h"
 #include "luaT.h"
 
@@ -15,7 +16,7 @@ extern "C"
 
 #define THFile_readRealRaw(file, data, size)                            \
   {                                                                     \
-    float *fdata = (float *)THAlloc(sizeof(float)*size);                         \
+    float *fdata = (float *)THAlloc(sizeof(float)*size);                \
     THFile_readFloatRaw(file, fdata, size);                             \
     THCampCheck(cudaMemcpy(data, fdata, size * sizeof(float), cudaMemcpyHostToDevice)); \
     THFree(fdata);                                                      \
@@ -23,7 +24,7 @@ extern "C"
 
 #define THFile_writeRealRaw(file, data, size)                           \
   {                                                                     \
-    float *fdata = (float *)THAlloc(sizeof(float)*size);                         \
+    float *fdata = (float *)THAlloc(sizeof(float)*size);                \
     THCampCheck(cudaMemcpy(fdata, data, size * sizeof(float), cudaMemcpyDeviceToHost)); \
     THFile_writeFloatRaw(file, fdata, size);                            \
     THFree(fdata);                                                      \
@@ -40,14 +41,14 @@ extern "C"
 /* now we overwrite some methods specific to CampStorage */
 
 #define CUDA_IMPLEMENT_STORAGE_COPY(TYPEC)                              \
-  static int clamptorch_##TYPEC##Storage_copy(lua_State *L)                \
-  {                                                                     \
+static int clamptorch_##TYPEC##Storage_copy(lua_State *L)               \
+{                                                                       \
     TH##TYPEC##Storage *storage = (TH##TYPEC##Storage *)luaT_checkudata(L, 1, "torch." #TYPEC "Storage"); \
     TH##TYPEC##Storage *src;                                                          \
-    if( (src = (TH##TYPEC##Storage *)luaT_toudata(L, 2, "torch." #TYPEC "Storage")) )         \
-      TH##TYPEC##Storage_copy(storage, src);                            \
+    if( (src = (TH##TYPEC##Storage *)luaT_toudata(L, 2, "torch." #TYPEC "Storage")) ) \
+        TH##TYPEC##Storage_copy(storage, src);                          \
     else                                                                \
-      luaL_typerror(L, 2, "torch.*Storage");                            \
+        luaL_typerror(L, 2, "torch.*Storage");                          \
     lua_settop(L, 1);                                                   \
     return 1;                                                           \
 }
@@ -61,42 +62,43 @@ CUDA_IMPLEMENT_STORAGE_COPY(Float)
 CUDA_IMPLEMENT_STORAGE_COPY(Double)
 CUDA_IMPLEMENT_STORAGE_COPY(Camp)
 }
+
 extern "C"
 {
 void clamptorch_CampStorage_init(lua_State* L)
 {
-  /* the standard stuff */
-  torch_CampStorage_init(L);
+    /* the standard stuff */
+    torch_CampStorage_init(L);
 
-  /* the copy methods */
-  {
-    int i;
-
-    const char* tnames[8] = {"torch.ByteStorage",
-                             "torch.CharStorage",
-                             "torch.ShortStorage",
-                             "torch.IntStorage",
-                             "torch.LongStorage",
-                             "torch.FloatStorage",
-                             "torch.DoubleStorage",
-                             "torch.CampStorage"};
-
-    static int (*funcs[8])(lua_State*) = {clamptorch_ByteStorage_copy,
-                                          clamptorch_CharStorage_copy,
-                                          clamptorch_ShortStorage_copy,
-                                          clamptorch_IntStorage_copy,
-                                          clamptorch_LongStorage_copy,
-                                          clamptorch_FloatStorage_copy,
-                                          clamptorch_DoubleStorage_copy,
-                                          clamptorch_CampStorage_copy};
-
-    for(i = 0; i < 8; i++)
+    /* the copy methods */
     {
-      luaT_pushmetatable(L, tnames[i]);
-      lua_pushcfunction(L, funcs[i]);
-      lua_setfield(L, -2, "copy");
-      lua_pop(L, 1);
+        int i;
+
+        const char* tnames[8] = {"torch.ByteStorage",
+                                 "torch.CharStorage",
+                                 "torch.ShortStorage",
+                                 "torch.IntStorage",
+                                 "torch.LongStorage",
+                                 "torch.FloatStorage",
+                                 "torch.DoubleStorage",
+                                 "torch.CampStorage"};
+
+        static int (*funcs[8])(lua_State*) = {clamptorch_ByteStorage_copy,
+                                              clamptorch_CharStorage_copy,
+                                              clamptorch_ShortStorage_copy,
+                                              clamptorch_IntStorage_copy,
+                                              clamptorch_LongStorage_copy,
+                                              clamptorch_FloatStorage_copy,
+                                              clamptorch_DoubleStorage_copy,
+                                              clamptorch_CampStorage_copy};
+
+        for(i = 0; i < 8; i++)
+        {
+              luaT_pushmetatable(L, tnames[i]);
+              lua_pushcfunction(L, funcs[i]);
+              lua_setfield(L, -2, "copy");
+              lua_pop(L, 1);
+        }
     }
-  }
 }
 }
