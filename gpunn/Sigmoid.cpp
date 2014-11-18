6725ec2aec5e72ea7390b441857c449a7b658878
Neelakandan <devnull@localhost>
2014-11-18 13:27:40 +0530

    C++ STL functions for Sigmoid function

--- gpunn/Sigmoid.cpp
+++ gpunn/Sigmoid.cpp
@@ -16,9 +16,13 @@ static int cunn_Sigmoid_updateOutput(lua_State *L)
 
   THCudaTensor_resizeAs(output, input);
 
-  /*thrust::device_ptr<float> output_data(THCudaTensor_data(output));
-  thrust::device_ptr<float> input_data(THCudaTensor_data(input));
-  thrust::transform(input_data, input_data+size, output_data, sigmoidupdateOutput_functor());*/
+   std::vector<float> output_data(THCudaTensor_data(output), THCudaTensor_data(output)+THCudaTensor_nElement(output));
+  //thrust::device_ptr<float> input_data(THCudaTensor_data(input));
+   std::vector<float> input_data(THCudaTensor_data(input), THCudaTensor_data(input)+THCudaTensor_nElement(input));
+ // thrust::transform(input_data, input_data+size, output_data, absupdateOutput_functor());
+   std::transform(input_data.begin(), input_data.end(), output_data.begin(), sigmoidupdateOutput_functor());
+
+   std::copy(output_data.begin(), output_data.end(), output->storage->data);
 
   THCudaTensor_free(input);
   return 1;
@@ -43,10 +47,17 @@ static int cunn_Sigmoid_updateGradInput(lua_State *L)
 
   THCudaTensor_resizeAs(gradInput, output);
 
- /* thrust::device_ptr<float> output_data(THCudaTensor_data(output));
-  thrust::device_ptr<float> gradOutput_data(THCudaTensor_data(gradOutput));
-  thrust::device_ptr<float> gradInput_data(THCudaTensor_data(gradInput));
-  thrust::transform(output_data, output_data+size, gradOutput_data, gradInput_data, sigmoidupdateGradInput_functor());*/
+   std::vector<float> output_data(THCudaTensor_data(output), THCudaTensor_data(output)+THCudaTensor_nElement(output));
+  //thrust::device_ptr<float> gradOutput_data(THCudaTensor_data(gradOutput));
+   std::vector<float> gradOutput_data(THCudaTensor_data(gradOutput), THCudaTensor_data(gradOutput)+THCudaTensor_nElement(gradOutput));
+  //thrust::device_ptr<float> gradInput_data(THCudaTensor_data(gradInput));
+   std::vector<float> gradInput_data(THCudaTensor_data(gradInput), THCudaTensor_data(gradInput)+THCudaTensor_nElement(gradInput));
+  //thrust::transform(input_data, input_data+size, gradOutput_data, gradInput_data, absupdateGradInput_functor());
+   std::transform(output_data.begin(), output_data.end(), gradOutput_data.begin(),gradInput_data.begin(), sigmoidupdateGradInput_functor());
+
+   
+   std::copy(gradInput_data.begin(), gradInput_data.end(), gradInput->storage->data);
+
 
   THCudaTensor_free(gradOutput);
   return 1;
