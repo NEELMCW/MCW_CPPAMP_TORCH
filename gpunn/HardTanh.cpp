2561970e604f9f427e90d455769761ede36fb06b
Neelakandan <devnull@localhost>
2014-11-19 11:52:13 +0530

    Redo C++ STL apis

--- gpunn/HardTanh.cpp
+++ gpunn/HardTanh.cpp
@@ -24,6 +24,13 @@ static int cunn_HardTanh_updateOutput(lua_State *L)
   /*thrust::device_ptr<float> output_data(THCudaTensor_data(output));
   thrust::device_ptr<float> input_data(THCudaTensor_data(input));
   thrust::transform(input_data, input_data+size, output_data, hardtanhupdateOutput_functor());*/
+  std::vector<float> output_data(THCudaTensor_data(output), THCudaTensor_data(output) + THCudaTensor_nElement(output));
+  //thrust::device_ptr<float> input_data(THCudaTensor_data(input));
+  std::vector<float> input_data(THCudaTensor_data(input), THCudaTensor_data(input) + THCudaTensor_nElement(input));
+ // thrust::transform(input_data, input_data+size, output_data, absupdateOutput_functor());
+  std::transform(input_data.begin(), input_data.end(), output_data.begin(), hardtanhupdateOutput_functor());
+ 
+  std::copy(output_data.begin(), output_data.end(),output->storage->data);
 
   THCudaTensor_free(input);
   return 1;
@@ -56,6 +63,15 @@ static int cunn_HardTanh_updateGradInput(lua_State *L)
   thrust::device_ptr<float> gradOutput_data(THCudaTensor_data(gradOutput));
   thrust::device_ptr<float> gradInput_data(THCudaTensor_data(gradInput));
   thrust::transform(input_data, input_data+size, gradOutput_data, gradInput_data, hardtanhupdateGradInput_functor());*/
+  std::vector<float> input_data(THCudaTensor_data(input), THCudaTensor_data(input) + THCudaTensor_nElement(input));
+  //thrust::device_ptr<float> gradOutput_data(THCudaTensor_data(gradOutput));
+  std::vector<float> gradOutput_data(THCudaTensor_data(gradOutput), THCudaTensor_data(gradOutput) + THCudaTensor_nElement(gradOutput));
+  //thrust::device_ptr<float> gradInput_data(THCudaTensor_data(gradInput));
+  std::vector<float> gradInput_data(THCudaTensor_data(gradInput), THCudaTensor_data(gradInput) + THCudaTensor_nElement(gradInput));
+  //thrust::transform(input_data, input_data+size, gradOutput_data, gradInput_data, absupdateGradInput_functor());
+  std::transform(input_data.begin(), input_data.end(), gradOutput_data.begin(),gradInput_data.begin(), hardtanhupdateGradInput_functor());
+
+  std::copy(gradInput_data.begin(), gradInput_data.end(),gradInput->storage->data);
 
   THCudaTensor_free(gradOutput);
   THCudaTensor_free(input);
