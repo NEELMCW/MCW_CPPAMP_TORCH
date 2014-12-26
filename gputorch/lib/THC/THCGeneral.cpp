92ad60608ac2b4138833a877c1bf8a19ce1d41aa
Neelakandan <devnull@localhost>
2014-12-26 11:12:32 +0530

    Share C++ AMP's context, queue and device with clBLAS apis

--- gputorch/lib/THC/THCGeneral.cpp
+++ gputorch/lib/THC/THCGeneral.cpp
@@ -1,6 +1,7 @@
 #include "THCGeneral.h"
 #include "TH.h"
 #include "THCTensorRandom.h"
+#include "cl_manage.h"
 
 cl_device_id mdevice=0;
 cl_context mcontext=0;
@@ -9,67 +10,16 @@ cl_command_queue mqueue=0;
 void THGPUInit()
 {
   int count = 0;
-  //THGPUCheck(gpuGetDeviceCount(&count));
-
   int device = 0;
-  //THGPUCheck(gpuGetDevice(&device));
 
-  /*state->rngState = (THGPURNGState*)malloc(sizeof(THGPURNGState));
-  THCRandom_init(state->rngState, count, device);
-
+  mdevice = Concurrency::getAllocator().device;
+  mcontext = Concurrency::getAllocator().context;
+  mqueue = Concurrency::getAllocator().getQueue();
   THGPUBlas_init(count, device);
-
-  int i,j;
-  for (i=0; i < count; ++i)
-  {
-    THGPUCheck(gpuSetDevice(i));
-    for (j=0; j < count; ++j)
-    {
-      if (i != j)
-      {
-        int can = 0;
-        THGPUCheck(gpuDeviceCanAccessPeer(&can, i, j));
-        if (can)
-          THGPUCheck(gpuDeviceEnablePeerAccess(j, 0));
-      }
-    }
-  }
-  THGPUCheck(gpuSetDevice(device));*/
-
-    cl_int err;
-    cl_platform_id platform = 0;
-    cl_context_properties props[3] = { CL_CONTEXT_PLATFORM, 0, 0 };
-    int ret = 0;
-    /* Setup OpenCL environment. */
-    err = clGetPlatformIDs(1, &platform, NULL);
-    if (err != CL_SUCCESS) {
-        printf( "clGetPlatformIDs() failed with %d\n", err );
-        return;
-    }
-    err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &mdevice, NULL);
-    if (err != CL_SUCCESS) {
-        printf( "clGetDeviceIDs() failed with %d\n", err );
-        return;
-    }
-    props[1] = (cl_context_properties)platform;
-    mcontext = clCreateContext(props, 1, &mdevice, NULL, NULL, &err);
-    if (err != CL_SUCCESS) {
-        printf( "clCreateContext() failed with %d\n", err );
-        return;
-    }
-    mqueue = clCreateCommandQueue(mcontext, mdevice, 0, &err);
-    if (err != CL_SUCCESS) {
-        printf( "clCreateCommandQueue() failed with %d\n", err );
-        clReleaseContext(mcontext);
-        return;
-    }
-    THGPUBlas_init(count, device);
 }
 
 void THGPUShutdown()
 {
-  //THCRandom_shutdown(state->rngState);
-  //free(state->rngState);
   THGPUBlas_shutdown();
 }
 
