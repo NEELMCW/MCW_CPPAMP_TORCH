898d6b56785d42733a5c81f7f209ed3568fb28f7
Sajini Ramesh <devnull@localhost>
2015-01-06 21:41:21 +0530

    fix: alternate GEMM implementation sequential

--- gputorch/lib/THC/gemm.cpp
+++ gputorch/lib/THC/gemm.cpp
@@ -1,29 +1,115 @@
 #include "gemm.h"
 
-// Stage 5: Highly parameterized GEMM
-int gemm(char TransA, char TransB, const int M, const int N, const int K, const float alpha,
-  float* A, const int lda, float* B, const int ldb,
-  const float beta, float* C, const int ldc)
+void gemm_NoTransAB(Concurrency::array_view<float, 1> &A, Concurrency::array_view<float, 1> &B, Concurrency::array_view<float, 1> &C, int M, int N, int K, int lda, int ldb, int ldc, float alpha, float beta)
+{
+  Concurrency::extent<1> grdExt(N * 16);
+  Concurrency::tiled_extent<16> t_ext(grdExt);
+  Concurrency::parallel_for_each(t_ext, [=] (Concurrency::tiled_index<16> tidx) restrict(amp){
+  float temp;
+  for (int j = tidx.global[0]; j < N; j+=t_ext[0]) {
+    if (beta == 0) {
+      for (int i = 0; i < M; ++i) {
+        C[i+j*ldc] = 0;
+      }
+    } else if (beta != 1) {
+      for (int i = 0; i < M; ++i) {
+        C[i+j*ldc] *= beta;
+      }
+    }
+    for (int l = 0; l < K; ++l) {
+      if (B[l+j*ldb] != 0) {
+        temp = alpha * B[l+j*ldb];
+        for (int i = 0; i < M; ++i) {
+          C[i+j*ldc] += A[i+l*lda] * temp;
+        }
+      }
+    }
+  }
+  });
+}
+
+void gemm_NoTransB(Concurrency::array_view<float, 1> &A, Concurrency::array_view<float, 1> &B, Concurrency::array_view<float, 1> &C, int M, int N, int K, int lda, int ldb, int ldc, float alpha, float beta)
+{
+  float temp;
+  for (int j = 0; j < N; ++j) {
+    for (int i = 0; i < M; ++i) {
+      temp = 0;
+      for (int l = 0; l < K; ++l) {
+        temp += A[l+i*lda] * B[l+j*ldb];
+      }
+      if (beta == 0) {
+        C[i+j*ldc] = alpha*temp;
+      } else {
+        C[i+j*ldc] = alpha*temp + beta*C[i+j*ldc];
+      }
+    }
+  }
+}
+
+void gemm_NoTransA(Concurrency::array_view<float, 1> &A, Concurrency::array_view<float, 1> &B, Concurrency::array_view<float, 1> &C, int M, int N, int K, int lda, int ldb, int ldc, float alpha, float beta)
+{
+  float temp;
+  for (int j = 0; j < N; ++j) {
+    if (beta == 0) {
+      for (int i = 0; i < M; ++i) {
+        C[i+j*ldc] = 0;
+      }
+    } else if (beta != 1) {
+      for (int i = 0; i < M; ++i) {
+        C[i+j*ldc] *= beta;
+      }
+    }
+    for (int l = 0; l < K; ++l) {
+      if (B[j+l*ldb] != 0) {
+        temp = alpha * B[j+l*ldb];
+        for (int i = 0; i < M; ++i) {
+          C[i+j*ldc] += A[i+l*lda] * temp;
+        }
+      }
+    }
+  }
+}
+
+void gemm_TransAB(Concurrency::array_view<float, 1> &A, Concurrency::array_view<float, 1> &B, Concurrency::array_view<float, 1> &C, int M, int N, int K, int lda, int ldb, int ldc, float alpha, float beta)
+{
+  float temp;
+  for (int j = 0; j < N; ++j) {
+    for (int i = 0; i < M; ++i) {
+      temp = 0;
+      for (int l = 0; l < K; ++l) {
+        temp += A[l+i*lda] * B[j+l*ldb];
+      }
+      if (beta == 0) {
+        C[i+j*ldc] = alpha*temp;
+      } else {
+        C[i+j*ldc] = alpha*temp + beta*C[i+j*ldc];
+      }
+    }
+  }
+}
+
+int gemm_AMP(char TransA, char TransB, const int M, const int N, const int K, const float alpha,
+  float* A, int lda, float* B, int ldb,
+  const float beta, float* C,  int ldc)
 {
   int num_rows_a, /*num_cols_a,*/ num_rows_b; // nrowa, ncola, nrowb
 
   // use longest possible type for intermediate value storage:
+  float temp;
   // %%= if [:rational,:complex,:value].include?(dtype.type); "#{dtype.long_dtype.sizeof} temp1, temp2;"; end%%
+  int i, j, l;
 
-  if (TransA == 'n') num_rows_a = M;
-  else                        num_rows_a = K;
+  if (TransA == 'n') 
+    num_rows_a = M;
+  else                        
+    num_rows_a = K;
 
-  if (TransB == 'n') num_rows_b = K;
-  else                        num_rows_b = N;
+  if (TransB == 'n') 
+     num_rows_b = K;
+  else                        
+     num_rows_b = N;
 
-  // Test the input parameters
- /* if (TransA < 111 || TransA > 113) {
-    fprintf(stderr, "GEMM: TransA must be CblasNoTrans, CblasTrans, or CblasConjTrans\n");
-    return 0;
-  } else if (TransB < 111 || TransB > 113) {
-    fprintf(stderr, "GEMM: TransB must be CblasNoTrans, CblasTrans, or CblasConjTrans\n");
-    return 0;
-  } else */if (M < 0) {
+  if (M < 0) {
     fprintf(stderr, "GEMM: Expected M >= 0\n");
     return 0;
   } else if (N < 0) {
@@ -42,167 +128,52 @@ int gemm(char TransA, char TransB, const int M, const int N, const int K, const 
     fprintf(stderr, "GEMM: Expected ldc >= max(1,M) with M=%d; got ldc=%d\n", M, ldc);
     return 0;
   }
-std::cout<<"0"<<std::endl;
+
   // Quick return if possible
   if (!M || !N || (alpha == 0 || !K) && beta == 1) return 0;
 
-
-  //Concurrency::array_view<float,1> A_mat(M * K, A);
-  //Concurrency::array_view<float,1> B_mat(K * N, B);
-  //Concurrency::array_view<float,1> C_mat(M * N, C);
-
-  //Concurrency::extent<1> grdExt(1);
-  //Concurrency::tiled_extent<1> t_ext(grdExt);
-
-
-  int i, j, l;
-  float temp;
   // For alpha = 0
   if (alpha == 0) {
     if (beta == 0) {
-      for (j = 0; j < N; j++)
+      for (j = 0; j < N; ++j)
         for (i = 0; i < M; ++i) {
           C[i+j*ldc] = 0;
         }
     } else {
-      for (j = 0; j < N; j++)
+      for (j = 0; j < N; ++j)
         for (i = 0; i < M; ++i) {
           C[i+j*ldc] *= beta;
         }
     }
     return 0;
   }
-  std::cout<<"1"<<std::endl;
   Concurrency::array_view<float,1> A_mat(M * K, A);
   Concurrency::array_view<float,1> B_mat(K * N, B);
   Concurrency::array_view<float,1> C_mat(M * N, C);
-
-  Concurrency::extent<1> grdExt(1);
-  Concurrency::tiled_extent<16> t_ext(grdExt);
   // Start the operations
   if (TransB == 'n') {
     if (TransA == 'n') {
       // C = alpha*A*B+beta*C
-  std::cout<<"2"<<std::endl;
-     Concurrency::parallel_for_each(t_ext, [=] (Concurrency::tiled_index<16> tidx) restrict(amp){
-      float temp;
-      for (int j = tidx.global[0]; j < N; j+=t_ext[0]) {
-        for (int l = 0; l < K; ++l) {
-            temp = alpha * B_mat[l+j*ldb];
-            for (int i = 0; i < M; ++i) {
-              C_mat[i+j*ldc] *= beta;
-              C_mat[i+j*ldc] += A_mat[i+l*lda] * temp;
-            }
-        }
-      }
-      });
+
+      gemm_NoTransAB(A_mat, B_mat, C_mat, M, N, K, lda, ldb, ldc, alpha, beta);
     } else {
 
       // C = alpha*A**T*B + beta*C
-  std::cout<<"3"<<std::endl;
-      Concurrency::parallel_for_each(t_ext, [=] (Concurrency::tiled_index<1> tidx) restrict(amp){
-      float temp;
-      for (int j = 0; j < N; ++j) {
-        for (int i = 0; i < M; ++i) {
-          temp = 0;
-          for (int l = 0; l < K; ++l) {
-            temp += A_mat[l+i*lda] * B_mat[l+j*ldb];
-          }
 
-          C_mat[i+j*ldc] = alpha*temp + beta*C_mat[i+j*ldc];
-        }
-      }
-      });
-
+      gemm_NoTransB(A_mat, B_mat, C_mat, M, N, K, lda, ldb, ldc, alpha, beta);
     }
 
   } else if (TransA == 'n') {
 
     // C = alpha*A*B**T + beta*C
 
-  std::cout<<"4"<<std::endl;
-    Concurrency::parallel_for_each(t_ext, [=] (Concurrency::tiled_index<1> tidx) restrict(amp){
-    float temp;
-    for (int j = 0; j < N; ++j) {
-      for (int l = 0; l < K; ++l) {
-          temp = alpha * B_mat[j+l*ldb];
-          for (int i = 0; i < M; ++i) {
-            C_mat[i+j*ldc] *= beta;
-            C_mat[i+j*ldc] += A_mat[i+l*lda] * temp;
-          }
-      }
-    }
-    });
-
+      gemm_NoTransA(A_mat, B_mat, C_mat, M, N, K, lda, ldb, ldc, alpha, beta);
   } else {
 
     // C = alpha*A**T*B**T + beta*C
-  std::cout<<"5"<<std::endl;
-    Concurrency::parallel_for_each(t_ext, [=] (Concurrency::tiled_index<1> tidx) restrict(amp){
-    float temp;
-    for (int j = 0; j < N; ++j) {
-      for (int i = 0; i < M; ++i) {
-        temp = 0;
-        for (int l = 0; l < K; ++l) {
-          temp += A_mat[l+i*lda] * B_mat[j+l*ldb];
-        }
 
-          C_mat[i+j*ldc] = alpha*temp + beta*C_mat[i+j*ldc];
-      }
-    }
-    });
+      gemm_TransAB(A_mat, B_mat, C_mat, M, N, K, lda, ldb, ldc, alpha, beta);
   }
+
   return 0;
 }
-/*void gemm_kernel(float alpha, float* a, float* b, float beta,  float* c, int m, int n, int k, int lda, int ldb, int ldc)
-{
-  //Concurrency::array_view<float,1> a_mat(m * k, a);
-  //Concurrency::array_view<float,1> b_mat(k * n, b);
-  //Concurrency::array_view<float,1> c_mat(m * n, c);
-  for (int i=0; i<m; i++) {
-               for (int j=0; j<n; j++) {
-                       c[i * m + j] *= beta;
-                       for (int p=0; p<k; p++) {
-                               c[i * m + j] += alpha * a[i * m + p] * b[p * k + j];
-                       }
-               }
-       }   
-}
-
-void gemm(int m, int n, int k, const char transa, const char transb, float alpha, float* a, float* b, float beta, float* c, int lda, int ldb, int ldc)  
-{ 
-  float *a_trans, *b_trans;
-  int m1, n1, k1;
-
-  if(transa == 't')
-  {
-    for(int i = 0; i < m; i++)
-      for(int j = 0; j < k; j++)
-        a_trans[i * k + j] = a[j * m + i];
-    m1 = k;
-    k1 = m;
-  }
-  else
-  {
-    a_trans = a;
-    m1 = m;
-    k1 = k;
-  }
-
-  if(transb == 't')
-  {
-    for(int i = 0; i < k; i++)
-      for(int j = 0; j < n; j++)
-        b_trans[i * n + j] = b[j * k + i];
-    k1 = n;
-    n1 = k;
-  }
-  else
-  {
-    b_trans = b;
-    k1 = k;
-    n1 = n;
-  }
-
-  gemm_kernel(alpha, a_trans, b_trans, beta, c, m1, n1, k1, lda, ldb, ldc);
-}*/
